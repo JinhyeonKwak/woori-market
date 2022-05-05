80ebe0f28fbcd6f6f19bd05ef0e1edfcb3fbd045
jinhyeon <vmflwu123@gmail.com>
2022-05-05 20:52:46 +0900

    feat(MarketService): 출석 관리 메서드 추가
    updateAttend 메서드 추가:
     - 장날이 되는 경우 해당 market의 모든 retailer에 대하여 출석 처리하는
    메서드
    
    checkAttend 메서드 추가:
     - 장에 출석하지 못하는 retailer가 있는 경우 결석 처리를 하는 메서드

--- src/main/java/com/mayy5/admin/service/MarketService.java
+++ src/main/java/com/mayy5/admin/service/MarketService.java
@@ -10,6 +10,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import java.time.DayOfWeek;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -62,7 +65,57 @@ public class MarketService {
         market.addRetailer(retailers);
     }
 
+    /**
+     * 장날이 되는 경우 출석 체크 필드 초기화
+     * @param marketId
+     * @param retailerId
+     */
+    @Transactional
+    public void updateAttend(Long marketId, Long retailerId) {
+        // 장 조회
+        Market market = marketRepository.findOne(marketId);
+        Retailer retailer = retailerRepository.findOne(retailerId);
 
+        // MarketRetailer 관계 엔티티 조회
+        List<MarketRetailer> marketRetailers = em.createQuery("select r from MarketRetailer r " +
+                        "where r.market = :market ", MarketRetailer.class)
+                .setParameter("market", market)
+                .getResultList();
+
+        // 장날이 되는 경우 모든 retailer의 출석 체크를 출석 처리
+        DayOfWeek marketDay = market.getMarketDay();
+        DayOfWeek today = LocalDateTime.now().getDayOfWeek();
+        if (marketDay == today) {
+            for (MarketRetailer marketRetailer : marketRetailers) {
+                em.createQuery("update MarketRetailer r set r.checkAttend = :attend where r = :marketRetailer")
+                        .setParameter("attend", CheckAttend.PRESENT)
+                        .setParameter("marketRetailer", marketRetailer); // 테스트 필요
+            }
+
+        }
+    }
+
+    /**
+     * 메서드 호출시 결석 처리
+     * @param marketId
+     * @param retailerId
+     */
+    @Transactional
+    public void checkAttend(Long marketId, Long retailerId) {
+        // 장 조회
+        Market market = marketRepository.findOne(marketId);
+        Retailer retailer = retailerRepository.findOne(retailerId);
+
+        // MarketRetailer 관계 엔티티 조회
+        MarketRetailer marketRetailer = em.createQuery("select r from MarketRetailer r where r.market = :market " +
+                        "and r.retailer = :retailer", MarketRetailer.class)
+                .setParameter("market", market)
+                .setParameter("retailer", retailer)
+                .getSingleResult(); // 조회 결과가 없는 경우 예외 처리 필요
+
+        // 결석 처리
+        marketRetailer.setCheckAttend(CheckAttend.ABSENT);
+    }
 
 
 }
