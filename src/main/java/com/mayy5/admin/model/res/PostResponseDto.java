8c425566718557bffb04ece158282ba0915e813a
boncheol.koo <boncheol.koo@navercorp.com>
2022-05-15 04:26:07 +0900

    fix: PostResponseDto 반환 값중 Comment를 CommentResponseDto로 수정
    - mapper에 default 로직 추가
    - Comment가 null임을 대비하여 Optional 처리

--- src/main/java/com/mayy5/admin/model/res/PostResponseDto.java
+++ src/main/java/com/mayy5/admin/model/res/PostResponseDto.java
@@ -3,9 +3,6 @@ package com.mayy5.admin.model.res;
 import java.time.LocalDateTime;
 import java.util.List;
 
-import com.mayy5.admin.model.domain.Comment;
-import com.mayy5.admin.model.domain.Post;
-import com.mayy5.admin.model.domain.User;
 import com.mayy5.admin.type.PostType;
 
 import lombok.AllArgsConstructor;
@@ -27,6 +24,6 @@ public class PostResponseDto {
 	private LocalDateTime createAt;
 	private LocalDateTime updateAt;
 	private String userId;
-	private List<Comment> comments;
+	private List<CommentResponseDto> comments;
 
 }
