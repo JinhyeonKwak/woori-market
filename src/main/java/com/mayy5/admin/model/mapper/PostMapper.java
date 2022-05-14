5344f9def246f7704e1fda6926374edc101365fb
boncheol.koo <boncheol.koo@navercorp.com>
2022-05-15 04:26:07 +0900

    fix: PostResponseDto 반환 값중 Comment를 CommentResponseDto로 수정
    - mapper에 default 로직 추가
    - Comment가 null임을 대비하여 Optional 처리

--- src/main/java/com/mayy5/admin/model/mapper/PostMapper.java
+++ src/main/java/com/mayy5/admin/model/mapper/PostMapper.java
@@ -1,13 +1,19 @@
 package com.mayy5.admin.model.mapper;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Named;
 import org.springframework.stereotype.Component;
 
+import com.mayy5.admin.model.domain.Comment;
 import com.mayy5.admin.model.domain.Post;
-import com.mayy5.admin.model.domain.User;
 import com.mayy5.admin.model.req.PostRequestDto;
+import com.mayy5.admin.model.res.CommentResponseDto;
 import com.mayy5.admin.model.res.PostResponseDto;
 
 @Component
@@ -16,8 +22,24 @@ public interface PostMapper {
 
 	Post toEntity(PostRequestDto rto);
 
+	@Mapping(source = "comments", target = "comments", qualifiedByName = "CommentToCommentDto")
 	@Mapping(source = "user", target = "userId", qualifiedByName = "UserToUserId")
 	PostResponseDto toDto(Post post);
+
+	@Named("CommentToCommentDto")
+	default List<CommentResponseDto> commentToCommentDto(final List<Comment> comments) {
+		return Optional.ofNullable(comments).orElse(new ArrayList<>())
+			.stream().map(comment ->
+				CommentResponseDto.builder()
+					.id(comment.getId())
+					.comment(comment.getComment())
+					.userId(comment.getUser().getId())
+					.createAt(comment.getCreateAt())
+					.updateAt(comment.getUpdateAt())
+					.postId(comment.getPost().getIdx())
+					.build()
+			).collect(Collectors.toList());
+	}
 
 }
 
