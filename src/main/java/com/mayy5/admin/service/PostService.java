e5248059388f7999e5b49eb608358437da88c781
boncheol.koo <boncheol.koo@navercorp.com>
2022-05-17 01:07:10 +0900

    feat: Post 수정/삭제 기능 추가
    - 해당 유저 or admin 유저만 삭제가 가능 (<- 현재는 admin id로 검사하는데 추후 대처할 수 있도록 userService.isValidUser로 검사
    - entity 값이 있는 애들에 한해서 기존 값에 Setting하는 로직을 추가

--- src/main/java/com/mayy5/admin/service/PostService.java
+++ src/main/java/com/mayy5/admin/service/PostService.java
@@ -14,6 +14,7 @@ import com.mayy5.admin.repository.CommentRepository;
 import com.mayy5.admin.repository.PostRepository;
 import com.mayy5.admin.repository.UserRepository;
 import com.mayy5.admin.type.PostType;
+import com.mayy5.admin.util.EntityUtil;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -43,12 +44,11 @@ public class PostService {
 		Comment comment = commentRepository.findById(commentId).orElseThrow(() ->
 			new CommonException(BError.NOT_EXIST, "Comment"));
 
-		if (userService.isValidUser(user,comment.getUser())) {
-			comment.setComment(input.getComment());
+		if (userService.isValidUser(user, comment.getUser())) {
+			return commentRepository.save(EntityUtil.setValueExceptNull(comment,input));
 		} else {
 			throw new CommonException(BError.NOT_MATCH, "User");
 		}
-		return commentRepository.save(comment);
 	}
 
 	@Transactional
@@ -68,7 +68,7 @@ public class PostService {
 			new CommonException(BError.NOT_EXIST, userId));
 
 		commentRepository.findById(commentId).ifPresent(comment -> {
-			if (userService.isValidUser(user,comment.getUser())) {
+			if (userService.isValidUser(user, comment.getUser())) {
 				commentRepository.deleteById(commentId);
 			} else {
 				throw new CommonException(BError.NOT_MATCH, "User");
@@ -83,6 +83,34 @@ public class PostService {
 			new CommonException(BError.NOT_EXIST, userId));
 		input.setUser(user);
 		return postRepository.save(input);
+	}
+
+
+	@Transactional
+	public void deletePost(String userId, Long postId) throws CommonException {
+		User user = userRepository.findById(userId).orElseThrow(() ->
+			new CommonException(BError.NOT_EXIST, userId));
+		postRepository.findById(postId).ifPresent(comment -> {
+			if (userService.isValidUser(user, comment.getUser())) {
+				postRepository.deleteById(postId);
+			} else {
+				throw new CommonException(BError.NOT_MATCH, "User");
+			}
+		});
+		return;
+	}
+
+	@Transactional
+	public Post updatePost(String userId, Long postId, Post input) {
+		User user = userRepository.findById(userId).orElseThrow(() ->
+			new CommonException(BError.NOT_EXIST, userId));
+		Post post = postRepository.findById(postId).orElseThrow(() ->
+			new CommonException(BError.NOT_EXIST, "POST"));
+		if (userService.isValidUser(user, post.getUser())) {
+			return postRepository.save(EntityUtil.setValueExceptNull(post, input));
+		} else {
+			throw new CommonException(BError.NOT_MATCH, "User");
+		}
 	}
 
 }
