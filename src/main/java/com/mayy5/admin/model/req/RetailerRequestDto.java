a217a2ccbc30ab0d05b08c0cd50aa4aee88709e0
boncheol.koo <boncheol.koo@navercorp.com>
2022-05-30 11:47:43 +0900

    refactor: 장 관리 API 리팩토링
    - 해당 코드 리팩토링 진행 (추가 진행 필요)
    - https://github.com/mayy5/plan/issues/9

--- src/main/java/com/mayy5/admin/model/req/RetailerRequestDto.java
+++ src/main/java/com/mayy5/admin/model/req/RetailerRequestDto.java
@@ -3,7 +3,11 @@ package com.mayy5.admin.model.req;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 import com.mayy5.admin.type.RetailerMetaType;
+import com.mayy5.admin.type.RetailerType;
 
 import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
@@ -17,9 +21,17 @@ import lombok.Setter;
 @NoArgsConstructor
 public class RetailerRequestDto {
 
+	@ApiModelProperty(value = "Market Location")
+	@NotBlank
+	private String name;
+
+	@ApiModelProperty(value = "Market Location")
+	@NotNull
+	private RetailerType type;
+
 	@ApiModelProperty(
 		value = "A RetailerPropType map of key/value pairs",
-		example = "{'BUSINESS_TYPE' : 'FOOD'}",
+		example = "{'ETC' : 'ETC'}",
 		dataType = "Map[String,String]")
 	private Map<RetailerMetaType, String> meta = new HashMap<>();
 
